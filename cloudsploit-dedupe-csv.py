9f842d376b87faf36afb015f0ae4e835f1b480f0
Jared Meit <j.meit@fwdsec.com>
2022-02-02 11:09:22 -0500

    Fetch extra info from cloudsploit github
    Added gitignore

--- cloudsploit-dedupe-csv.py
+++ cloudsploit-dedupe-csv.py
@@ -4,6 +4,8 @@ import csv
 import argparse
 import pathlib
 from os import path
+import requests
+import re
 
 parser = argparse.ArgumentParser( description='Deduplicate CloudSploit report findings and filter for only FAILed issues. Does not delete or modify orignal file.' )
 parser.add_argument(
@@ -43,18 +45,42 @@ def main():
                 issues[ row[ cols.index('title') ] ]['Description'] = row[cols.index('description')]
                 issues[ row[ cols.index('title') ] ]['Resources and Regions'] = ""
                 issues[ row[ cols.index('title') ] ]['Message'] = row[cols.index('message')]
+                for key,content in cloudsploit_guide( row[cols.index('category')], row[cols.index('title')] ).items():
+                    issues[ row[ cols.index('title') ] ][key] = content
             
             issues[ row[ cols.index('title') ] ]['Resources and Regions'] += resource_region
             
     deduped_file = f'deduped-{path.basename( csv_file )}'
     with open(deduped_file, 'w') as dd:
-        dd.write( f"Category,Title,Description,Resources and Regions, Message,\n"  )
+        dd.write( f"Category,Title,Description,Resources and Regions, Message, More Info, Azure Link, Remediation\n"  )
         for title in issues:
             for col in issues[title]:
                 dd.write( f'"{issues[title][col]}",'  )
             dd.write( "\n" )
 
     print( f"Success! Deduped file: ./{deduped_file}" )
+
+
+def cloudsploit_guide( cat, title ):
+
+    guide = dict()
+    cat_f = cat.lower().replace( " ", "" )
+    title_f = title.lower().replace( " ", "-" )
+
+    res = requests.get( f'https://raw.githubusercontent.com/aquasecurity/cloud-security-remediation-guides/master/en/azure/{cat_f}/{title_f}.md' )
+    
+    if res.status_code != 200:
+        return guide
+
+    rows = [
+        "More Info",
+        "AZURE Link",
+        "Recommended Action"
+    ]
+    for r in rows:
+        guide[ r ] = re.search( f"\| \*\*{r}\*\* \| ([^|]+)", res.text )[1]
+
+    return guide
 
 
 main()
